9e6c3b1b1ed26963a4e918cd02233ebe7b8fcdfa
Yug <03yugagarwal@gmail.com>
2024-11-13 22:33:55 +0530

    Refactor ScheduleChat and Navbar components; enhance styles and responsiveness

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,47 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 import '../styles/Navbar.css';
 
 const Navbar = () => {
-  const handleLogout = () => {
-    localStorage.removeItem("token");
-    window.location.reload();
-  };
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-  const loginButton = localStorage.getItem("token") ? (
-    <li>
-      <button onClick={handleLogout}>LOGOUT</button>
-    </li>
-  ) : (
-    <li>
-      <NavLink to="/login">LOGIN</NavLink>
-    </li>
-  );
+    const handleLogout = () => {
+        localStorage.removeItem("token");
+        window.location.reload();
+    };
 
-  return (
-    <div className="navbar_container">
-      <div className="navbar_logo">
-        <Link to="/">
-          <img src="MindfulHub_LatestLogo.png" alt="MindfulHub Logo" />
-        </Link>
-        <h1 className="navbar_header">
-          <Link to="/" className="navbar_link">MindfulHub</Link>
-        </h1>
-      </div>
-      <ul className="navbar_ul">
-        <li>
-          <NavLink to="/blog">BLOGS</NavLink>
-        </li>
-        <li>
-          <NavLink to="/questionnaireForm">TEST</NavLink>
-        </li>
-        <li>
-          <NavLink to="/chat">CHAT</NavLink>
-        </li>
-        {loginButton}
-      </ul>
-    </div>
-  );
+    const loginButton = localStorage.getItem("token") ? (
+        <li><button className="buttonNavbar" onClick={handleLogout}>LOGOUT</button></li>
+    ) : (
+        <li><NavLink to="/login">LOGIN</NavLink></li>
+    );
+
+    return (
+        <div className="navbar_container">
+            <div className="navbar_logo">
+                <Link to="/">
+                    <img src="MindfulHub_LatestLogo.png" alt="MindfulHub Logo" />
+                </Link>
+                <h1 className="navbar_header">
+                    <Link to="/" className="navbar_link">MindfulHub</Link>
+                </h1>
+            </div>
+            
+            {/* Hamburger Icon */}
+            <div className="hamburger_icon" onClick={() => setIsMenuOpen(!isMenuOpen)}>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+            
+            {/* Navbar Links */}
+            <ul className={`navbar_ul ${isMenuOpen ? 'open' : ''}`}>
+                <li><NavLink to="/blog">BLOGS</NavLink></li>
+                <li><NavLink to="/questionnaireForm">TEST</NavLink></li>
+                <li><NavLink to="/chat">CHAT</NavLink></li>
+                {loginButton}
+            </ul>
+        </div>
+    );
 };
 
 export default Navbar;
--- client/src/components/ScheduleChat.jsx
+++ client/src/components/ScheduleChat.jsx
@@ -25,21 +25,21 @@ const ScheduleChat = () => {
         setMessage('');
         setIsAnonymous(false);
 
-        alert('Scheduled Data Send Successfully!');
+        alert('Scheduled Data Sent Successfully!');
     };
 
     return (
         <div className="schedulechat">
+            <div className="schedulechat_header">
+                <h1>Schedule a Chat</h1>
+                <h4>
+                    Schedule a chat with a psychology expert to get help, help someone, or ask questions.
+                </h4>
+            </div>
             <div className="schedulechat_container">
                 <div className="box">
                     <div className="schedulechat_left">
                         <img src="SchedueleChat.jpg" alt="Background" />
-                        <div className="left-content">
-                            <h1>Schedule a Chat</h1>
-                            <h4>
-                                Schedule a chat with a psychology expert to get help, help someone, or ask questions.
-                            </h4>
-                        </div>
                     </div>
                     <div className="schedulechat_right">
                         <form onSubmit={handleScheduleChat}>
